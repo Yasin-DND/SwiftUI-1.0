c0834ba5beac6fdd987b12cb84de7d08c162d1d1
Yasin <yasinmemon482@yahoo.com>
2020-08-07 23:04:16 +0500

    Initial Commit

---
+++ SwiftUI 1.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI 1.0
+//
+//  Created by Mac on 07/08/2563 BE.
+//  Copyright © 2563 DND. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
